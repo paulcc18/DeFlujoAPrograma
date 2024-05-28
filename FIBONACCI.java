ee57d46a0ccea388721c60672eab1e612f6a1d3c
paulcc18 <gpc1808@gmail.com>
2024-05-28 10:36:24 -0500

    Add Fibonacci code

---
+++ FIBONACCI.java
@@ -0,0 +1,22 @@
+package DeFlujoaPrograma;
+
+import java.util.Scanner;
+
+public class FIBONACCI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\tHow many terms do you want to generate?:");
+        int numberCases = scanner.nextInt();
+
+        int firstNumber = 0, secondNumber = 1;
+        System.out.println("\tThe series for " + numberCases + " terms:");
+
+        for (int i = 0; i < numberCases; i++) {
+            System.out.print("    "+ firstNumber );
+            int auxiliar = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = auxiliar;
+        }
+        scanner.close();
+    }
+}
